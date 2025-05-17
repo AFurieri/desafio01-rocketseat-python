45a418f0548c8c4ebd5b2a09e9bc0ffed42ece74
Arthur furieri <art.furieri@gmail.com>
2025-05-17 09:48:32 -0300

    funcionalidade editar contato adicionada ao projeto

--- main.py
+++ main.py
@@ -5,17 +5,45 @@ def adicionar_contato(contatos, nome, telefone, email):
                "email": email,
                "favorito": False
                }
+    
     contatos.append(contato)
+
     print(f"contato de {nome} adicionado com sucesso")
+
     return
+
 
 def visualizar_contatos(contatos):
     for i, contato in enumerate(contatos, start=1):
         favorito = "*" if contato["favorito"] else ""
-        print(f"{i} {contato["nome"]}; {contato["telefone"]}; {contato["email"]} {favorito}")
+        print(f"{i} {contato["nome"]} / {contato["telefone"]} / {contato["email"]} {favorito}")
+    
     return
 
+
+def editar_contato(contatos, indice, alterar):
+    indice -= 1
+
+    if alterar == "1":
+        mudanca = input("digite o novo nome ")
+        contatos[indice]["nome"] = mudanca
+
+    elif alterar =="2":
+        mudanca = input("digite o novo numero ")
+        contatos[indice]["telefone"] = mudanca
+
+    elif alterar =="3":
+        mudanca = input("digite o novo email ")
+        contatos[indice]["email"] = mudanca
+
+    else:
+        print("alternativa inválida")
+    
+    return
+
+
 lista_contatos = []
+
 
 while True:
     print("\nlista de contatos\n")
@@ -30,13 +58,28 @@ while True:
     escolha = input("digite a sua escolha: ")
 
     if escolha == "1":
-        nome = input("digite o nome do contato que voce deseja adicionar")
-        telefone = input("digite o telefone do contato que voce deseja adicionar")
-        email = input("digite o email do contato que voce deseja adicionar")
+        nome = input("digite o nome do contato que voce deseja adicionar ")
+        telefone = input("digite o telefone do contato que voce deseja adicionar ")
+        email = input("digite o email do contato que voce deseja adicionar ")
         adicionar_contato(lista_contatos, nome, telefone, email)
     
     elif escolha == "2": 
         visualizar_contatos(lista_contatos)
+
+    elif escolha == "3":
+        visualizar_contatos(lista_contatos)
+
+        contato = int(input("digite o indice (numero que fica a frente) do contato que voce deseja atualizar "))
+
+        print("1. nome")
+        print("2. telefone")
+        print("3. email")
+        alterar = input("digite o que voce deseja alterar no contato ")
+        editar_contato(lista_contatos, contato, alterar)
+        
+
+            
+
     
 
 
